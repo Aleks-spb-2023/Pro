e9572671a3e0917ca63cfb099e6361583b8e67f5
Sasha <sasha@example.com>
2022-11-08 16:00:38 +0300

    int

---
+++ Array/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите количество слов");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] Array = new string[n];
+for (int i = 0; i < n; i++)
+{   
+    string str = Console.ReadLine();
+    Array[i] = str;
+}
+Console.WriteLine( "[" + string.Join(",",Array)+"]" );

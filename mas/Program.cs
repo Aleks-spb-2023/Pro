93570211025abf57599066e6cdd8e58d1d138ce8
Sasha <sasha@example.com>
2022-11-08 16:28:15 +0300

    Ver1

---
+++ mas/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите количество слов");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] Array = new string[n];
+for (int i = 0; i < n; i++)
+{   
+    string S = Console.ReadLine();
+    Array[i] = S;
+}
+Console.WriteLine( "[" + string.Join(",",Array)+"]" );
